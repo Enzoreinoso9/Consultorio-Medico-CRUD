a30444291fb36e3a5e286401c847e0a45c4df82d
enzo <enzoexe999@gmail.com>
2024-01-20 18:00:54 -0300

    first commit

---
+++ config/connection.php
@@ -0,0 +1,14 @@
+<?php 
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = ""; //Ingresar base de datos
+
+
+$conn = new mysqli($host,$username,$password,$database);
+
+if (!$conn) {
+    die("Conexión fallida: ".$conn ->connect_error);
+}
+?>
